35f08453cf551a25e52d5a3b7dd30d816aefc668
dongheon0827 <dongheon827@naver.com>
2024-05-02 17:06:13 +0900

    줄 정리

--- src/main/kotlin/bncMain.kt
+++ src/main/kotlin/bncMain.kt
@@ -3,18 +3,15 @@ package org.example
 fun bncMain(gameRecord: BncRecord) {
     val randNum = MakeAnswer()
     val userInput = UserInput()
-    println("게임을 시작합니다. 숫자를 입력하세요")
     var count = 0
+    println("게임을 시작합니다. 숫자를 입력하세요")
+
     while (true) {
         var strike = 0
         var ball = 0
 
         val userNum = userInput.userInputList()
-
         count++
-
-//        println(userNum)
-//        println(randNum)
 
         for ((index, num) in userNum.withIndex()) {
             if (randNum[index] == num) {
@@ -24,10 +21,11 @@ fun bncMain(gameRecord: BncRecord) {
             }
         }
         if (strike == 0 && ball == 0) println("Nothing")
+
         else println("${count}번째, ${strike}스트라이크, ${ball}볼")
         if (strike == 3) {
             println("정답입니다")
-            gameRecord.addRecord(count)
+            gameRecord.add(count)
             break
         }
     }
