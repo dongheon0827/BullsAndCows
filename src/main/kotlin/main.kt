299e5a23b057ac3d58a61ed201f114413c5dc5d5
dongheon0827 <dongheon827@naver.com>
2024-05-02 17:08:13 +0900

    키워드 축약된 함수 이름 사용

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -6,13 +6,15 @@ fun main() {
     while (true) {
         println("1.게임 시작하기 2.게임 기록 보기 3.종료하기")
         val selectNum: Int = readln().toInt()
+
         when (selectNum) {
             1 -> bncMain(gameRecord)
-            2 -> gameRecord.printRecord()
+            2 -> gameRecord.print()
             3 -> {
                 println("숫자 야구 게임을 종료합니다")
                 return
             }
+
             else -> {
                 println("올바른 숫자를 입력해주세요")
             }
